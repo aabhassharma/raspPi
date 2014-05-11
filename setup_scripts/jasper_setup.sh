21f3a0ad0e444726e05aa5b3454c93e0195d6238
Aabhas Sharma <aabhas.sharma@gmail.com>
2014-05-10 21:15:07 -0700

    Jasper set up script 1

---
+++ setup_scripts/jasper_setup.sh
@@ -0,0 +1,20 @@
+#Helper script for Jasper setup
+#@author : Aabhas Sharma - aabhas.sharma@gmail.com
+#NOTE : I do not own Jasper, and have nothing to do with the team
+#that created it. Please visit http://jasperproject.github.io/ for more information
+
+#!/bin/sh
+# Download the required tars, untar them
+wget http://downloads.sourceforge.net/project/cmusphinx/sphinxbase/0.8/sphinxbase-0.8.tar.gz && \
+wget http://downloads.sourceforge.net/project/cmusphinx/pocketsphinx/0.8/pocketsphinx-0.8.tar.gz && \
+tar -zxvf sphinxbase-0.8.tar.gz && \
+tar -zxvf pocketsphinx-0.8.tar.gz && \
+cd ~/sphinxbase-0.8/
+# install sphinxbase
+./configure --enable-fixed
+make && sudo make install
+# install pocketsphinx
+cd ~/pocketsphinx-0.8/
+./configure
+make && sudo make install
+echo "All done!"
